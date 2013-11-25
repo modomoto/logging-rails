05dbabfc1e1f87a2c17a7789b977e287638024a5
Mathias Fiedler <mathias@musterdenker.de>
2013-11-25 18:46:29 +0100

    setting some defaults

--- lib/logging/rails/generators/install_generator.rb
+++ lib/logging/rails/generators/install_generator.rb
@@ -11,14 +11,17 @@ module Logging::Rails
 
       def insert_log_to_destinations
         comment = "\n  # Set the logging destination(s)\n  %s\n"
-        insert_into_file 'config/environments/development.rb', comment % 'config.log_to = %w[stdout file]', :before => %r/^end\s*$/
+        insert_into_file 'config/environments/development.rb', comment % 'config.log_to = %w[file]', :before => %r/^end\s*$/
         insert_into_file 'config/environments/production.rb', comment % 'config.log_to = %w[file]', :before => %r/^end\s*$/
       end
 
       def insert_show_log_configuration
         comment = "\n  # Show the logging configuration on STDOUT\n  config.show_log_configuration = %s\n"
-        insert_into_file 'config/environments/development.rb', comment % 'true', :before => %r/^end\s*$/
+        insert_into_file 'config/environments/development.rb', comment % 'false', :before => %r/^end\s*$/
         insert_into_file 'config/environments/production.rb', comment % 'false', :before => %r/^end\s*$/
+
+        insert_into_file 'config/environments/development.rb', "\n config.log_level = :info", :before => %r/^end\s*$/
+        insert_into_file 'config/environments/production.rb',  "\n config.log_level = :event", :before => %r/^end\s*$/
       end
     end
   end
